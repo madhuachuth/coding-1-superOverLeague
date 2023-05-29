95d8d549497e51a759557fca1218ff9e8133d4fc
madhuachuth <madhuachuth7@gmail.com>
2023-05-29 07:51:19 +0000

    superOver

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="main-heading">Super Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="image"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
